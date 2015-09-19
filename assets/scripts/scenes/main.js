537d0d8ee2d58d0555702c1c1687b7702b98d047
phi <phi1618jp@gmail.com>
2015-09-19 23:21:11 +0900

    fix main

--- assets/scripts/scenes/main.js
+++ assets/scripts/scenes/main.js
@@ -8,8 +8,24 @@ phina.define('MainScene', {
     this.superInit();
 
     this.fromJSON({
-
+      children: {
+        gaugeShape: {
+          className: 'GaugeShape',
+        },
+        stage: {
+          className: 'CanvasElement',
+        },
+        arrowGroup: {
+          className: 'CanvasElement',
+        },
+        ui: {
+          className: 'CanvasElement',
+        },
+      },
     });
+
+    this.timer = 0;
+    this.limitTime = TIME;
   },
 
   onenter: function() {
@@ -19,20 +35,28 @@ phina.define('MainScene', {
       count: ['Ready'],
       fontSize: 100,
     });
-
-    this.app.pushScene(scene);
-
     scene.onexit = function() {
       // this.setQuestion();
     }.bind(this);
-  }
+
+    this.app.pushScene(scene);
+  },
+
+  update: function() {
+    this.addTime(this.app.ticker.deltaTime);
+  },
+
+  addTime: function(time) {
+    this.timer += time;
+    this.gaugeShape.value = (this.timer/this.limitTime);
+  },
 });
 
 /*
  * 最初の円のフィルター
  */
-phina.define("CircleFilterEffect", {
-    superClass: "Shape",
+phina.define('CircleFilterEffect', {
+    superClass: 'Shape',
 
     init: function(options) {
       this.superInit({
@@ -58,10 +82,53 @@ phina.define("CircleFilterEffect", {
     update: function() {
         var c = this.canvas;
 
-        c.context.globalCompositeOperation = "destination-out";
+        c.context.globalCompositeOperation = 'destination-out';
         c.fillCircle(0, 0, this.circleRadius);
     },
 });
 
 
+
+phina.define('GaugeShape', {
+  superClass: 'Shape',
+
+  init: function() {
+    this.superInit({
+      width: SCREEN_WIDTH,
+      height: SCREEN_HEIGHT,
+      backgroundColor: 'hsla(0, 0%, 80%, 0.5)',
+    });
+
+    this.origin.y = 1;
+    this.setPosition(SCREEN_CENTER_X, SCREEN_HEIGHT);
+
+    this.value = 0.0;
+  },
+
+  update: function() {
+    if (this.value >= 0.9) {
+      var rate = 1-((1.0-this.value)/0.1);
+      var s = rate*100;
+      var color = 'hsla(0, {0}%, 80%, 0.5)'.format(s);
+      this.style.backgroundColor = color;
+    }
+  },
+
+  _accessor: {
+    value: {
+      get: function() {
+        return this._value;
+      },
+      set: function(v) {
+        this._value = v;
+        this.scaleY = this.value;
+      },
+    },
+  },
+
+  // setValue: function(rate) {
+  //     this.value = Math.clamp(rate, 0, 1);
+  //     this.scaleY = this.value;
+  // },
+});
 
