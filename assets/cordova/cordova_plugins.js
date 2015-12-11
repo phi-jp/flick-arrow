4a30bbeb5d601c214eae6b1aec257b00863fbddf
phi <phi1618jp@gmail.com>
2015-12-11 15:48:04 +0900

    add cordova files

---
+++ assets/cordova/cordova_plugins.js
@@ -0,0 +1,38 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/cordova-plugin-statusbar/www/statusbar.js",
+        "id": "cordova-plugin-statusbar.statusbar",
+        "pluginId": "cordova-plugin-statusbar",
+        "clobbers": [
+            "window.StatusBar"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-admobpro/www/AdMob.js",
+        "id": "cordova-plugin-admobpro.AdMob",
+        "pluginId": "cordova-plugin-admobpro",
+        "clobbers": [
+            "window.AdMob"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-game-center/www/gamecenter.js",
+        "id": "cordova-plugin-game-center.GameCenter",
+        "pluginId": "cordova-plugin-game-center",
+        "clobbers": [
+            "gamecenter"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "cordova-plugin-whitelist": "1.2.0",
+    "cordova-plugin-statusbar": "2.0.0",
+    "cordova-plugin-extension": "1.2.2",
+    "cordova-plugin-admobpro": "2.9.9",
+    "cordova-plugin-game-center": "0.4.2"
+}
+// BOTTOM OF METADATA
+});
