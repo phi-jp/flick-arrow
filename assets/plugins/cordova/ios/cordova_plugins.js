5be289ac5fcc34990b035354db4996b4bcc68fdc
phi <phi1618jp@gmail.com>
2015-12-22 18:20:42 +0900

    fix plugin

--- assets/plugins/cordova/ios/cordova_plugins.js
+++ assets/plugins/cordova/ios/cordova_plugins.js
@@ -57,11 +57,11 @@ module.exports = [
         ]
     },
     {
-        "file": "plugins/cordova-plugin-app-version/www/AppVersionPlugin.js",
-        "id": "cordova-plugin-app-version.AppVersionPlugin",
-        "pluginId": "cordova-plugin-app-version",
-        "clobbers": [
-            "cordova.getAppVersion"
+        "file": "plugins/cordova-plugin-appinfo/www/appinfo.js",
+        "id": "cordova-plugin-appinfo.AppInfo",
+        "pluginId": "cordova-plugin-appinfo",
+        "merges": [
+            "navigator.appInfo"
         ]
     }
 ];
@@ -78,7 +78,7 @@ module.exports.metadata =
     "cordova-plugin-crosswalk-webview": "1.4.0",
     "cordova-plugin-extension": "1.2.3",
     "cordova-plugin-admobpro": "2.10.0",
-    "cordova-plugin-app-version": "0.1.8"
+    "cordova-plugin-appinfo": "2.0.2"
 }
 // BOTTOM OF METADATA
 });
---
+++ assets/plugins/cordova/ios/plugins/cordova-plugin-appinfo/www/appinfo.js
@@ -0,0 +1,28 @@
+cordova.define("cordova-plugin-appinfo.AppInfo", function(require, exports, module) { 
+var exec = require('cordova/exec');
+
+module.exports = {
+
+    getAppInfo: function(success, fail){
+        exec(success, fail, 'AppInfo', 'getAppInfo', []);
+    },
+
+    /**
+     * Returns the version name (or "unknown" if it fails)
+     *
+     * @param {Function} callback       The message to accept the version name.
+     */
+    getVersion: function(callback) {
+        exec(callback, function(err) {
+        	callback('Unknown');
+    	}, 'AppInfo', 'getVersion', []);
+    },
+
+    getIdentifier: function(callback){
+        exec(callback, function(err){
+            callback('Unknown');
+        }, 'AppInfo', 'getIdentifier', []);
+    }
+};
+
+});
